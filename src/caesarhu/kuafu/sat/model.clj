07642d9709a3c56c07808561e1664202b2efb1c4
caesarhu <hupeishun@gmail.com>
2022-10-24 06:41:04 +0800

    update sat/model

--- src/caesarhu/kuafu/sat/model.clj
+++ src/caesarhu/kuafu/sat/model.clj
@@ -67,6 +67,70 @@
   [^CpModel model expr lb ub]
   (.addLinearConstraint model expr lb ub))
 
+(defn add-equality
+  [^CpModel model expr value]
+  (.addEquality model expr value))
+
+(defn add-less-or-equal
+  [^CpModel model expr value]
+  (.addLessOrEqual model expr value))
+
+(defn add-less-than
+  [^CpModel model expr value]
+  (.addLessThan model expr value))
+
+(defn add-greater-or-equal
+  [^CpModel model expr value]
+  (.addGreaterOrEqual model expr value))
+
+(defn add-greater-than
+  [^CpModel model expr value]
+  (.addGreaterThan model expr value))
+
+(defn add-different
+  [^CpModel model expr value]
+  (.addDifferent model expr value))
+
+(defn add-all-different
+  [^CpModel model expressions]
+  (.addAllDifferent model (into-array expressions)))
+
+(defn add-element
+  [^CpModel model index values target]
+  (.addElement model index values target))
+
+(defn add-circuit
+  [^CpModel model]
+  (.addCircuit model))
+
+(defn add-multiple-circuit
+  [^CpModel model]
+  (.addMultipleCircuit model))
+
+(defn add-allowed-assignments
+  [^CpModel model variables]
+  (.addAllowedAssignments model (into-array variables)))
+
+(defn add-forbidden-assignments
+  [^CpModel model variables]
+  (.addForbiddenAssignments model (into-array variables)))
+
 (defn add-automaton
   [^CpModel model transitionVariables startingState finalStates]
   (.addAutomaton model (into-array transitionVariables) startingState (long-array finalStates)))
+
+(defn add-inverse
+  [^CpModel model variables inverseVariables]
+  (.addInverse model (into-array variables) (into-array inverseVariables)))
+
+(defn addReservoirConstraint
+  [^CpModel model minLevel maxLevel]
+  (.addReservoirConstraint model minLevel maxLevel))
+
+(defn add-map-domain
+  [^CpModel model var booleans offset]
+  (.addMapDomain model var (into-array booleans) offset))
+
+(defn addMinEquality
+  [^CpModel model target exprs]
+  (.addMinEquality model target (into-array exprs)))
