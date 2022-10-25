712bdd9b280229a785d10c3b9ac5828e325cb55b
caesarhu <hupeishun@gmail.com>
2022-10-26 03:00:52 +0800

    update sat/model.clj

--- src/caesarhu/kuafu/sat/model.clj
+++ src/caesarhu/kuafu/sat/model.clj
@@ -23,6 +23,42 @@
 (defmethod int-var [Long Long] [^CpModel model lb ub]
   (.newIntVar model lb ub (rand-name)))
 
+(defn new-fixed-interval
+  ([^CpModel model start size name]
+   (.newFixedInterval model start size name))
+  ([^CpModel model start size]
+   (.newFixedInterval model start size (rand-name))))
+
+(defn interval-var
+  ([^CpModel model start size end name]
+   (.newIntervalVar model start size end name))
+  ([^CpModel model start size end]
+   (.newIntervalVar model start size end (rand-name))))
+
+(defn fixed-size-interval-var
+  ([^CpModel model start size name]
+   (.newFixedSizeIntervalVar model start size name))
+  ([^CpModel model start size]
+   (.newFixedSizeIntervalVar model start size (rand-name))))
+
+(defn new-optional-interval-var
+  ([^CpModel model start size end isPresent name]
+   (.newOptionalIntervalVar model start size end isPresent name))
+  ([^CpModel model start size end isPresent]
+   (.newOptionalIntervalVar model start size end isPresent (rand-name))))
+
+(defn new-optional-fixed-size-interval-var
+  ([^CpModel model start size isPresent name]
+   (.newOptionalFixedSizeIntervalVar model start size isPresent name))
+  ([^CpModel model start size isPresent]
+   (.newOptionalFixedSizeIntervalVar model start size isPresent (rand-name))))
+
+(defn new-optional-fixed-interval
+  ([^CpModel model start size isPresent name]
+   (.newOptionalFixedInterval model start size isPresent name))
+  ([^CpModel model start size isPresent]
+   (.newOptionalFixedInterval model start size isPresent (rand-name))))
+
 (defn true-literal
   [^CpModel model]
   (.trueLiteral model))
@@ -131,6 +167,104 @@
   [^CpModel model var booleans offset]
   (.addMapDomain model var (into-array booleans) offset))
 
-(defn addMinEquality
+(defn add-min-equality
   [^CpModel model target exprs]
   (.addMinEquality model target (into-array exprs)))
+
+(defn add-max-equality
+  [^CpModel model target exprs]
+  (.addMaxEquality model target (into-array exprs)))
+
+(defn add-division-equality
+  [^CpModel model target exprs num denom]
+  (.addDivisionEquality model target exprs num denom))
+
+(defn add-abs-equality
+  [^CpModel model target expr]
+  (.addAbsEquality model target expr))
+
+(defn add-modulo-equality
+  [^CpModel model var mod]
+  (.addModuloEquality model var mod))
+
+(defn add-multiplication-equality
+  [^CpModel model target exprs]
+  (if (coll? exprs)
+    (.addMultiplicationEquality model target (into-array exprs))
+    (.addMultiplicationEquality model target exprs)))
+
+(defn add-no-overlap
+  [^CpModel model intervalVars]
+  (.addNoOverlap model (into-array intervalVars)))
+
+(defn add-no-overlap-2d
+  [^CpModel model]
+  (.addNoOverlap2D model))
+
+(defn add-cumulative
+  [^CpModel model capacity]
+  (.addCumulative model capacity))
+
+(defn add-hint
+  [^CpModel model var value]
+  (.addHint model var value))
+
+(defn clear-hints
+  [^CpModel model]
+  (.clearHints model))
+
+(defn add-assumption
+  [^CpModel model lit]
+  (.addAssumption model lit))
+
+(defn add-assumptions
+  [^CpModel model literals]
+  (.addAssumption model (into-array literals)))
+
+(defn clear-assumptions
+  [^CpModel model]
+  (.clearAssumptions model))
+
+(defn minimize
+  [^CpModel model expr]
+  (.minimize model expr))
+
+(defn maximize
+  [^CpModel model expr]
+  (.maximize model expr))
+
+(defn clear-objective
+  [^CpModel model]
+  (.clearObjective model))
+
+(defn has-objective
+  [^CpModel model]
+  (.hasObjective model))
+
+(defn add-decision-strategy
+  [^CpModel model variables varStr domStr]
+  (.addDecisionStrategy model variables varStr domStr))
+
+(defn model-stats
+  [^CpModel model]
+  (.modelStats model))
+
+(defn validate
+  [^CpModel model]
+  (.validate model))
+
+(defn export-to-file
+  [^CpModel model ^String file]
+  (.exportToFile model file))
+
+(defn model
+  [^CpModel model]
+  (.model model))
+
+(defn negated
+  [^CpModel model index]
+  (.negated model index))
+
+(defn get-builder
+  [^CpModel model]
+  (.getBuilder model))
